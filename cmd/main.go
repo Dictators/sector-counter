272eff0fc90f608a915bafee6ed5e0f13b5927fa
Dictators <1182756906@qq.com>
2021-10-23 15:38:45 +0800

    second commit

--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,8 @@ import (
 	"os"
 	"time"
 
-	client "github.com/moran666666/sector-counter/client"
-	server "github.com/moran666666/sector-counter/server"
+	client "github.com/Dictators/sector-counter/client"
+	server "github.com/Dictators/sector-counter/server"
 )
 
 func main() {
--- server/server.go
+++ server/server.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"sync"
 
-	pb "github.com/moran666666/sector-counter/proto"
+	pb "github.com/Dictators/sector-counter/proto"
 
 	"google.golang.org/grpc"
 )
